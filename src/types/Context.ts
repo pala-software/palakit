f0b9a257ac4f1e71a70234d053985b120b38d47d
Jesse Sivonen <j.sivonen@hotmail.com>
2024-05-08 21:47:24 +0300

    Make context properties optional

--- src/types/Context.ts
+++ src/types/Context.ts
@@ -2,6 +2,6 @@ import type { Collection } from "./Collection";
 import type { FieldType } from "./FieldType";
 
 export interface Context<T extends Context<any> = any> {
-  fieldTypes: Record<string, FieldType>;
-  collections: Record<string, Collection<T>>;
+  fieldTypes?: Record<string, FieldType>;
+  collections?: Record<string, Collection<T>>;
 }
