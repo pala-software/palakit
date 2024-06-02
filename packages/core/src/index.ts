547303fccc2cc4fdf0f1498edd93a11b589412f6
Jesse Sivonen <j.sivonen@hotmail.com>
2024-06-02 10:34:59 +0300

    Fix index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,3 @@
 export { Runtime, resolveRuntime } from "./runtime";
-export { createTrigger } from "./trigger";
-export { createFunction } from "./function";
 export { createPart } from "part-di";
 export { EventBus, Subscription } from "./eventBus";
