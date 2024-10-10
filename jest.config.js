a22d8d4a6e5d9aabdd59f23a09d706a74d05147f
Joona Kauppinen <joona.kauppinen98@gmail.com>
2024-10-10 18:05:46 +0300

    Remove unnecessary configs

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,4 @@ import { createJsWithTsEsmPreset } from "ts-jest";
 /** @type {import("ts-jest").JestConfigWithTsJest} */
 export default {
   ...createJsWithTsEsmPreset(),
-  transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
-  },
 };
