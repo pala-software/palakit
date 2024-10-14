f855bba088347e0d3d645446edf9db91a76b93d1
Joona Kauppinen <joona.kauppinen98@gmail.com>
2024-10-14 16:10:16 +0300

    Default implementation for Logger

--- packages/core/src/Logger.ts
+++ packages/core/src/Logger.ts
@@ -12,4 +12,12 @@ export type Logger = {
   };
 };
 
-export const Logger = createPart<Logger>("Logger");
+export const Logger = createPart<Logger>("Logger", [], () => ({
+  createLogger: (category: string) => ({
+    debug: (obj, msg) => console.debug(category, obj, msg),
+    info: (obj, msg) => console.info(category, obj, msg),
+    warn: (obj, msg) => console.warn(category, obj, msg),
+    error: (obj, msg) => console.error(category, obj, msg),
+    fatal: (obj, msg) => console.error(category, obj, msg),
+  }),
+}));
