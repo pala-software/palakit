6419d94521911dd138743c0903ae8399ce6148c1
Joona Kauppinen <joona.kauppinen98@gmail.com>
2024-10-17 10:22:22 +0300

    Fix file name

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,6 @@ export { Application, resolveApplication } from "./Application";
 export type { ApplicationOptions } from "./Application";
 export { createConfiguration, createFeature } from "./Feature";
 export { LocalRuntime } from "./LocalRuntime";
-export { Logger } from "./Logger.js";
+export { Logger } from "./Logger";
 export { Runtime } from "./Runtime";
 export type { Function, Trigger } from "./Runtime";
